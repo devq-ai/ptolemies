11cb8b65c90b4952f43d90b2e07c8d6c2946ab63
dion edge <dion@wrench.chat>
2025-06-25 03:41:39 -0500

    Add all missing status page lib components
    - Force add all lib files that were being ignored
    - Include Incidents, Status, System components
    - Add logs.json and types.ts files
    - Status page should now build successfully

---
+++ status-page/src/lib/types.ts
@@ -0,0 +1,21 @@
+export interface ReportFile {
+	site: Array<{
+		name: string;
+		status: Array<{ timestamp: number; result: boolean }>;
+	}>;
+	incidents: Array<{ date: number; title: string; open: boolean }>;
+}
+
+export interface Status {
+	status: StatusCode;
+	date: Date;
+}
+
+export enum StatusCode {
+	/** No error reported */
+	OK = 0,
+	/** Server currently unreachable/throwing > 400 errors */
+	ERROR = 1,
+	/** Server was throwing error but now it is fixed */
+	UNSTABLE = -1
+}
